e37c736afb5dba3e145a5661284d4e9c4781786f
S.Maham <syedahmaham@gmail.com>
2021-06-21 12:42:55 -0400

    new h1

--- src/PieceOfArt.js
+++ src/PieceOfArt.js
@@ -2,6 +2,7 @@ function PieceOfArt({longTitle, imgSrc, title}) {
   // const {longTitle, imgPath, title} = props;
   return (
     <div>
+      <h1>welcome peeepsss</h1>
       <h2>{longTitle}</h2>
       <img src={imgSrc} alt={title} />
     </div>
